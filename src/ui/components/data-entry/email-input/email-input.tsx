f84a838587cafaee593b61cd3f9981545af8c501
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-07-02 15:13:14 -0300

    fix(scalars): move hook inside of ui folder and add memo for maxLength property

--- src/ui/components/data-entry/email-input/email-input.tsx
+++ src/ui/components/data-entry/email-input/email-input.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { FormDescription } from '../../../../scalars/components/fragments/form-description/index.js'
 import { FormGroup } from '../../../../scalars/components/fragments/form-group/index.js'
 import { FormLabel } from '../../../../scalars/components/fragments/form-label/index.js'
 import { FormMessageList } from '../../../../scalars/components/fragments/form-message/index.js'
 import { Input } from '../input/index.js'
-import useControllableState from '../../../../scalars/hooks/use-controllable-state.js'
 import type { EmailInputProps } from './types.js'
 import { useUniqueId } from '../../../../scalars/lib/utils.js'
+import useControllableState from '../../../hooks/use-controllable-state.js'
 
 const EmailInput = React.forwardRef<HTMLInputElement, EmailInputProps>(
   (
@@ -39,7 +39,10 @@ const EmailInput = React.forwardRef<HTMLInputElement, EmailInputProps>(
     const autoCompleteValue = autoComplete === true ? 'email' : autoComplete === false ? 'off' : undefined
 
     // Filter maxLength from props since it's avoid default browser behavior
-    const filteredProps = Object.fromEntries(Object.entries(props).filter(([key]) => key !== 'maxLength'))
+    const filteredProps = useMemo(
+      () => Object.fromEntries(Object.entries(props).filter(([key]) => key !== 'maxLength')),
+      [props]
+    )
 
     return (
       <FormGroup>
