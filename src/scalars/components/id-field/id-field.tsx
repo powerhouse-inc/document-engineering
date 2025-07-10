3d20b66cec300ab3333a33d0ce04ee9fbc6a924b
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-07-10 18:21:12 -0300

    fix(scalars): fix submit id in id-field correct-way get id email

--- src/scalars/components/id-field/id-field.tsx
+++ src/scalars/components/id-field/id-field.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { nanoid } from 'nanoid'
 
@@ -15,25 +15,29 @@ export interface IdFieldProps {
 export const IdField: React.FC<IdFieldProps> = ({ name = 'id', value, generator = 'nanoid', ...rest }) => {
   const {
     register,
+    setValue,
     formState: { submitCount },
   } = useFormContext()
-  const actualValue = useMemo(
-    () => {
-      if (value !== undefined) return value
 
-      if (typeof generator === 'function') return generator()
+  const actualValue = useMemo(() => {
+    if (value !== undefined) return value
 
-      switch (generator) {
-        case 'nanoid':
-          return nanoid()
-        case 'uuid':
-          return crypto.randomUUID()
-      }
-    },
+    if (typeof generator === 'function') return generator()
+
+    switch (generator) {
+      case 'nanoid':
+        return nanoid()
+      case 'uuid':
+        return crypto.randomUUID()
+    }
     // We want to re-generate the ID on every submit
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [value, generator, submitCount]
-  )
+  }, [value, generator, submitCount])
+
+  // Sync the generated ID with react-hook-form when actualValue changes (submitCount)
+  useEffect(() => {
+    setValue(name, actualValue)
+  }, [setValue, name, actualValue])
 
   return <input type="hidden" value={actualValue} {...register(name)} {...rest} />
 }
