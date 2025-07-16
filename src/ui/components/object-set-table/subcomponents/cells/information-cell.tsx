a851e0ebb55eff61beb9a27ba99d09330b73ad34
Yasiel Cabrera <yasiel9506@gmail.com>
2025-07-15 20:33:18 -0400

    feat(table): move delete options to the secondary row actions

--- src/ui/components/object-set-table/subcomponents/cells/information-cell.tsx
+++ src/ui/components/object-set-table/subcomponents/cells/information-cell.tsx
@@ -1,34 +1,7 @@
-import { Icon } from '../../../icon/icon.js'
-import { useInternalTableState } from '../table-provider/table-provider.js'
 import { TableCellBasic } from './basic-cell.js'
 
-interface InformationCellProps {
-  rowIndex: number
-  emptyRow?: boolean
-}
-
-const InformationCell = ({ rowIndex, emptyRow }: InformationCellProps) => {
-  const {
-    state: { selectedRowIndexes },
-    api,
-  } = useInternalTableState()
-
-  const canDelete = api.canDelete()
-
-  return (
-    <TableCellBasic className="min-w-[40px] max-w-[40px]">
-      {!emptyRow && canDelete && selectedRowIndexes.includes(rowIndex) && (
-        <div
-          className="flex flex-1 w-full items-center justify-center cursor-pointer text-red-900"
-          onClick={() => {
-            void api.deleteRows([rowIndex])
-          }}
-        >
-          <Icon name="Trash" size={16} />
-        </div>
-      )}
-    </TableCellBasic>
-  )
+const InformationCell = () => {
+  return <TableCellBasic className="min-w-[40px] max-w-[40px]" />
 }
 
 export { InformationCell }
