57d032e8d3a99cff3e70dcb661cf5a0aacc5096a
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-08-21 13:05:02 -0300

    feat(scalars): add hook handle logic for each file

---
+++ src/ui/components/data-entry/file-input/sub-components/placeholder-corrupted.tsx
@@ -0,0 +1,37 @@
+import { cn } from '../../../../../scalars/lib/utils.js'
+import { Icon } from '../../../icon/index.js'
+import type { PreviewType } from '../types.js'
+import { getBrokenFileIcon } from '../utils.js'
+import PreviewHeader from './preview-header.js'
+
+interface PlaceHolderCorruptedProps {
+  onClose: () => void
+  className?: string
+  typePreview: PreviewType
+}
+
+export const PlaceHolderCorrupted = ({ onClose, className, typePreview }: PlaceHolderCorruptedProps) => {
+  const iconBroken = getBrokenFileIcon(typePreview)
+
+  return (
+    <div
+      className={cn(
+        'flex flex-col bg-white rounded-md shadow-[1px_4px_15px_0px_rgba(74,88,115,0.25)] px-6 py-4 gap-4 h-[652px] w-[500px]',
+        className
+      )}
+    >
+      <PreviewHeader status="error" onClose={onClose} title="Corrupted File" />
+      <div className="flex flex-col items-center justify-center bg-gray-100 w-full rounded-md overflow-hidden flex-1">
+        <div className="relative">
+          <Icon name={iconBroken} size={128} className="text-gray-300" />
+        </div>
+      </div>
+
+      <div className=" border-gray-200">
+        <p className="text-xs text-red-900 font-normal leading-4.5">
+          Your file is corrupted and cannot be shown in the preview
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/ui/components/data-entry/file-input/sub-components/preview-audio.tsx
+++ src/ui/components/data-entry/file-input/sub-components/preview-audio.tsx
@@ -1,36 +1,29 @@
 import { cn } from '../../../../../scalars/lib/utils.js'
-import { PREVIEW_STATUS, type PreviewStatus } from '../types.js'
-import { PlaceHolderAudio } from './placeholder-audio.js'
 import PreviewHeader from './preview-header.js'
 
 interface AudioPreviewProps {
   onClose?: () => void
   preview?: string
   className?: string
-  status: PreviewStatus
 }
 
-const PreviewAudio = ({ status, onClose = () => null, preview, className }: AudioPreviewProps) => {
-  if (status === PREVIEW_STATUS.SUCCESS && preview) {
-    return (
-      <div
-        className={cn(
-          'flex flex-col bg-white rounded-md shadow-[1px_4px_15px_0px_rgba(74,88,15,0.25)] px-6 py-4 gap-4 w-full',
-          className
-        )}
-      >
-        <PreviewHeader status={status} onClose={onClose} title="Audio Preview" />
+const PreviewAudio = ({ onClose = () => null, preview, className }: AudioPreviewProps) => {
+  return (
+    <div
+      className={cn(
+        'flex flex-col bg-white rounded-md shadow-[1px_4px_15px_0px_rgba(74,88,15,0.25)] px-6 py-4 gap-4 w-full',
+        className
+      )}
+    >
+      <PreviewHeader status="idle" onClose={onClose} title="Audio Preview" />
 
-        <div className="flex-1 flex items-center justify-center">
-          <audio src={preview} controls className="w-full" aria-label="Audio player">
-            <p>Your browser does not support the audio preview.</p>
-          </audio>
-        </div>
+      <div className="flex-1 flex items-center justify-center">
+        <audio src={preview} controls className="w-full" aria-label="Audio player">
+          <p>Your browser does not support the audio preview.</p>
+        </audio>
       </div>
-    )
-  }
-
-  return <PlaceHolderAudio status={status} onClose={onClose} className={cn('w-full h-full', className)} />
+    </div>
+  )
 }
 
 export default PreviewAudio
